35f12750a99c17059c11f0dd17934eb7f4c8c452
Lukasz Piatkowski <lukaspiatkowski@gmail.com>
2023-09-14 11:26:30 +0200

    wave: add query to the DocumentNode for logging

---
+++ graphql_server/document_with_query.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from graphql import DocumentNode
+
+
+class DocumentNodeWithQuery(DocumentNode):
+    __slots__ = ("query",)
+
+    query: str
+
+    @staticmethod
+    def from_document_node(
+        node: DocumentNode,
+        query: str
+    ) -> DocumentNodeWithQuery:
+        return DocumentNodeWithQuery(
+            loc=node.loc,
+            definitions=node.definitions
+            query=query
+        )
